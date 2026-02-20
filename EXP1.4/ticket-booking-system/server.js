87df4e2792d95d127560948047adb6ca5ab89496
Aryan24sa <aryan1305thakur@gmail.com>
2026-02-20 10:42:43 +0530

    Add files via upload

---
+++ EXP1.4/ticket-booking-system/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bookingSystem = require("./booking-system");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Server Working 🚀");
+});
+
+app.post("/api/book", bookingSystem.bookSeat);
+
+app.listen(3000, () => {
+  console.log("Ticket Booking System running on port 3000");
+});
